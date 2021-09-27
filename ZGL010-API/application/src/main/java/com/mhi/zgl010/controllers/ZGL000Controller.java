1a7b406e1b4f7eedea4ebc5551cf2969187dc945
Shiena Hirashima <shirashima@tohmatsu.co.jp>
2021-09-27 08:31:21 +0000

    modify

--- ZGL010-API/application/src/main/java/com/mhi/zgl010/controllers/ZGL000Controller.java
+++ ZGL010-API/application/src/main/java/com/mhi/zgl010/controllers/ZGL000Controller.java
@@ -9,17 +9,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 import com.mhi.zgl010.models.HelloWorldResponse;
 
 @RestController
-@RequestMapping( "/hello" )
 public class ZGL000Controller
 {
     private static final Logger logger = LoggerFactory.getLogger(ZGL000Controller.class);
 
-    @RequestMapping( method = RequestMethod.GET )
+    @RequestMapping( path = "/hello",method = RequestMethod.GET )
     public ResponseEntity<HelloWorldResponse> getHello( @RequestParam( defaultValue = "world" ) final String name )
     {
         logger.info("I am running!");
